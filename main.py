3b44e3bd671392705ea201065fcc097f3ba32060
Mads3132 <mwsa17@student.aau.dk>
2021-10-05 10:59:25 +0200

    Testing

---
+++ main.py
@@ -0,0 +1,57 @@
+#Set-up a singular node
+class Node:
+    def __init__(self, dataval=None):
+        self.dataval = dataval
+        self.nextval = None
+
+#Create a single linked list
+class SLinkedList:
+    def __init__(self):
+            self.headval = None
+
+with open('players.txt') as f:
+    content = f.read()
+    words = content.splitlines()
+    scontent = sorted(words)
+    #print(scontent)
+
+list1 = SLinkedList()
+list1.headval = Node(scontent[0])
+i = 0
+while i < len(scontent):
+    i += 1
+    e2 = Node(scontent[i])
+    e3 = Node("Wed")
+
+#Link first node to second node
+list1.headval.nextval = e2
+
+#Link second node to thrid node
+e2.nextval = e3
+
+#Member function to print list in console
+def listprint(self):
+    printval = self.headval
+    while printval is not None:
+        print (printval.dataval)
+        printval = printval.nextval
+
+#Member to insert at beginning of node
+def AtBegining(self, newdata):
+    NewNode = Node(newdata)
+#Update the new nodes next val to existing node
+    NewNode.nextval = self.headval
+    self.headval = NewNode
+
+#Member to insert at the end of node
+def AtEnd(self, newdata):
+    NewNode = Node(newdata)
+    if self.headval is None:
+        self.headval = NewNode
+        return
+    laste = self.headval
+    while(laste.nextval):
+        laste = laste.nextval
+    laste.nextval=NewNode
+
+listprint(list1)
